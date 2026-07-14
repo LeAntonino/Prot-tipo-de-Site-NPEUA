be729c6d37a4b46ad022124390d4c3df8c7bef16
Antonino <121946938+LeAntonino@users.noreply.github.com>
2026-07-14 17:34:50 -0300

    alterada fonte para ficar preta durante manhã

--- main/javascript/global.js
+++ main/javascript/global.js
@@ -7,11 +7,14 @@ function relogio(){
     
     
     if(hora >= 6 && hora < 12 ){
-        main.style.backgroundImage = "url('imagens/background_morning.png')";         
+        main.style.backgroundImage = "url('imagens/background_morning.png')";
+        main.style.color = '#000000';         
     }else if(hora >= 12 && hora < 18){
         main.style.backgroundImage = "url('imagens/background_afternoon.png')";
+        main.style.color = '#ffffff';
     }else{
         main.style.backgroundImage = "url('imagens/background.png')";
+        main.style.color = '#ffffff';
     }
 }
 relogio()
